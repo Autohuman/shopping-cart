4d574cdfc9fe2cf960d5078fc117b8fb72dc0e4c
Autohuman <2788039381@qq.com>
2018-12-01 16:01:15 +0800

    16:01

--- js/body.js
+++ js/body.js
@@ -29,3 +29,29 @@ function contains(arr, obj) {
     }
     return false;
 }
+
+/*店铺下商品全选框功能*/
+let storeGroup = document.getElementsByName("shopCheck");
+
+for (let i = 0; i < storeGroup.length; i++) {
+    storeGroup[i].onclick = function() {
+    let status = storeGroup[i].checked;
+    let sss = storeGroup[i].parentNode.parentNode.parentNode.childNodes[3].childNodes;
+    let arr = new Array(3,5,7,9,11,13,15,17,19,21,23,25);
+    for (let m = 1; m < sss.length; m++) {
+      if (contains(arr,m)) {
+        let laa = sss[m].childNodes[1].childNodes[0];
+        if (status) {
+            laa.checked = true;
+        }
+        else {
+            laa.checked = false;
+            let selectAllToChange = document.getElementsByClassName('selectAll');
+            for (let s = 0; s < selectAllToChange.length; s++) {
+              selectAllToChange[s].checked = false;
+            }
+        }
+      }
+    } 
+  }
+}
