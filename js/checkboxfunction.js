8e0b5a7e1916d6c2f2f5bee3ff375ea29b5dea38
Autohuman <2788039381@qq.com>
2018-12-02 14:05:51 +0800

    plus and minus button function all finished

--- js/checkboxfunction.js
+++ js/checkboxfunction.js
@@ -33,19 +33,16 @@ function contains(arr, obj) {
 }
 
 /*店铺下商品全选框功能*/
-
-
 for (let i = 0; i < storeGroup.length; i++) {
     storeGroup[i].onclick = function() {
     let status = storeGroup[i].checked;
-    let sss = storeGroup[i].parentNode.parentNode.parentNode.childNodes[3].childNodes;
+    let sss = storeGroup[i].parentNode.parentNode.parentNode.childNodes[1].childNodes;
     // SSS为从商家复选框到其对应下的所有具体商品的复选框的“映射”的第一个阶段
-    let arr = new Array(3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59);
+    let arr = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59];
     for (let m = 1; m < sss.length; m++) {
       if (contains(arr,m)) {
         // LAA为从SSS到具体某商家下对应的所有复选框的“映射”，选取到所有对应的复选框
-        let laa = sss[m].childNodes[1].childNodes[0];
-        console.log(laa);
+        let laa = sss[m].firstChild.firstChild;
         if (status) {
             laa.checked = true;
         }
@@ -74,34 +71,9 @@ for (let i = 0; i < storeGroup.length; i++) {
     }
   }
 }
+
+
 /*具体某件商品的复选框状态设置*/
-
-
-for (let i = 0; i < singleCommidityGroup.length; i++) {
-
-    singleCommidityGroup[i].onchange = function() {
-    let commidityCheckCount = 0;
-    let commidityContainer = new Array();
-    let commidityProjectionFirst = singleCommidityGroup[i].parentNode.parentNode.parentNode.getElementsByTagName('ul');
-    for (let m = 0; m < commidityProjectionFirst.length; m++) {
-      let commidityProjectionSecond = commidityProjectionFirst[m].childNodes[1].firstChild;
-      commidityContainer.push(commidityProjectionSecond);
-    }
-    for (let m= 0; m < commidityContainer.length; m++) {
-      if (commidityContainer[m].checked == true) {
-        commidityCheckCount++;
-      }
-    }
-    if (commidityCheckCount === commidityContainer.length) {
-      singleCommidityGroup[i].parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].firstChild.checked = true;
-      findWhentoChange()
-    }
-    else if (commidityCheckCount == 0) {
-      singleCommidityGroup[i].parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[1].firstChild.checked = false;
-      findWhentoChange()
-
-    }
-
 function findWhentoChange(){
       let storeCheckCount = 0;
       for (let m = 0; m < storeGroup.length; m++) {
@@ -115,5 +87,34 @@ function findWhentoChange(){
         }
       }
   }
-}
+
+
+for (let i = 0; i < singleCommidityGroup.length; i++) {
+    singleCommidityGroup[i].onchange = function(event) {
+    let commidityCheckCount = 0;
+    let commidityContainer = new Array();
+    let commidityProjectionFirst = singleCommidityGroup[i].parentNode.parentNode.parentNode.getElementsByTagName('ul');
+    if (event.target.checked == false) {
+      for (let n = 0; n < group.length; n++) {
+        group[n].checked = false;
+      }
+    }
+    for (let m = 0; m < commidityProjectionFirst.length; m++) {
+      let commidityProjectionSecond = commidityProjectionFirst[m].childNodes[0].firstChild;
+      commidityContainer.push(commidityProjectionSecond);
+    }
+    for (let m= 0; m < commidityContainer.length; m++) {
+      if (commidityContainer[m].checked == true) {
+        commidityCheckCount++;
+      }
+    }
+    if (commidityCheckCount === commidityContainer.length) {
+      singleCommidityGroup[i].parentNode.parentNode.parentNode.parentNode.firstChild.firstChild.firstChild.checked = true;
+      findWhentoChange()
+    }
+    else if (commidityCheckCount == 0) {
+      singleCommidityGroup[i].parentNode.parentNode.parentNode.parentNode.firstChild.firstChild.firstChild.checked = false;
+      findWhentoChange()
+    }
+  }
 }
--- js/variable.js
+++ js/variable.js
@@ -1,27 +1,81 @@
-let shopGroup = [{id:13579,name:"京东自营"}];   /*店铺列表*/
-let commidityGroup = [{src:"你猜啊",name:"良心商家良心商家良心商家良心商家良心商家",info:"颜色；赤橙黄绿青蓝紫.大小：超级大",price:19999.00,shopid:13579,ordercount:1}];    /*商品列表*/
+let shopGroup = [{id:13579,name:"京东自营"},{id:12345,name:"郝轲"}];   /*店铺列表*/
+let commidityGroup = [{id:123456789,src:"images/tamako.jpg",name:"良心商家良心商家良心商家良心商家良心商家",info:"颜色；赤橙黄绿青蓝紫.大小：超级大",price:19999.00,shopid:13579,ordercount:1},{id:12345678,src:"images/CommodityItemImg1.jpg",name:"无敌是多么的寂寞无敌是多么的寂寞无敌是多么的寂寞",info:"无敌是多么的寂寞无敌是多么的寂寞",price:19999.00,shopid:12345,ordercount:10},{id:123456789,src:"images/tamako.jpg",name:"良心商家良心商家良心商家良心商家良心商家",info:"颜色；赤橙黄绿青蓝紫.大小：超级大",price:19999.00,shopid:13579,ordercount:1},{id:12345678,src:"images/CommodityItemImg1.jpg",name:"无敌是多么的寂寞无敌是多么的寂寞无敌是多么的寂寞",info:"无敌是多么的寂寞无敌是多么的寂寞",price:19999.00,shopid:12345,ordercount:10}];    /*商品列表*/
+// let commidityGroup = [];
+if (commidityGroup.length == 0) {
+  document.querySelector("#body-cart").innerHTML += "<div class='alertInfo'>“购物车中没有商品，快去添加吧”</div>";
+}
+else{
+  for (let i = 0; i < shopGroup.length; i++) {
+    let object = "<div class='shopGoods'>";
+    let shopInfoOjb = "<div class='shopCategory'><span><input class='checkbox' name='shopCheck' type='checkbox' /><p class='shopname'>";
+    let endSignal = "</div>";
+    let commidityInfoObjSum = "";
+
+    shopInfoOjb += shopGroup[i].name;
+    shopInfoOjb += "</p></span></div>";
 
 
-for (let i = 0; i < shopGroup.length; i++) {
-  let object = "<div class='shopGoods'>";
-  let shopInfoOjb = "<div class='shopCategory'><span><input class='checkbox' name='shopCheck' type='checkbox' /><p class='shopname'>";
-  let endSignal = "</div>";
+    for (let m = 0; m < commidityGroup.length; m++) {
+      if (commidityGroup[m].shopid == shopGroup[i].id) {
+        let commidityInfoObj = "<div class='commodity' id='";
+        commidityInfoObj += commidityGroup[m].id;
+        commidityInfoObj += "'><!--同一商家下添加商品，添加一个ul列表--><ul class='commodityList'><li class='CommodityList1'><input name='selectCommodity' class='checkbox' type='checkbox' /></li><li class='CommodityList2'><div class='CommodityItemImg'><img class='CommodityItemImage' src='";
+        commidityInfoObj += commidityGroup[m].src;
+        commidityInfoObj += "' /></div><a class='CommodityItemName' href='#'>";
+        commidityInfoObj += commidityGroup[m].name;
+        commidityInfoObj += "</a><div class='CommodityItemInfo'>";
+        commidityInfoObj += commidityGroup[m].info;
+        commidityInfoObj += "</div></li><li class='CommodityList3'>￥";
+        commidityInfoObj += commidityGroup[m].price;
+        commidityInfoObj += "</li><li class='CommodityList4'><button class='button button-minus button-square button-tiny'><i class='fa fa-minus'></i></button><input type='text' class='commodityNumInput' value='";
+        commidityInfoObj += commidityGroup[m].ordercount;
+        commidityInfoObj += "' /><button class='button button-square button-plus button-tiny'><i class='fa fa-plus'></i></button></li><li class='CommodityList5'>￥51996.00<br/>22.88kg</li><li class='CommodityList6'><a class='#'>删除商品</a><a class='#'>加入我的关注</a></li></ul></div>";
+        commidityInfoObjSum += commidityInfoObj;
+        }
+      }
+    object += shopInfoOjb + commidityInfoObjSum + endSignal;
+    document.querySelector("#body-cart").innerHTML += object;
+    }
+}
 
-  shopInfoOjb += shopGroup[i].name;
-  shopInfoOjb += "</p></span></div>";
-  document.querySelector("#body-cart").innerHTML += object;
-  document.querySelector("#body-cart").innerHTML += shopInfoOjb;
-  for (let m = 0; m < commidityGroup.length; m++) {
-    if (commidityGroup[m].shopid == shopGroup[i].id) {
-      let commidityInfoObj = "<div class='commodity'><!--同一商家下添加商品，添加一个ul列表--><ul class='commodityList'><li class='CommodityList1'><input name='selectCommodity' class='checkbox' type='checkbox' /></li><li class='CommodityList2'><div class='CommodityItemImg'><img class='CommodityItemImage' src='images/CommodityItemImg1.jpg' /></div><a class='CommodityItemName' href='#'>";
-      commidityInfoObj += commidityGroup[m].name;
-      commidityInfoObj += "</a><div class='CommodityItemInfo'>";
-      commidityInfoObj += commidityGroup[m].info;
-      commidityInfoObj += "</div></li><li class='CommodityList3'>￥";
-      commidityInfoObj += commidityGroup[m].price;
-      commidityInfoObj += "</li><li class='CommodityList4'><button class='button button-square button-tiny'><i class='fa fa-minus'></i></button><input type='text' class='commodityNumInput' /><button class='button button-square button-tiny'><i class='fa fa-plus'></i></button></li><li class='CommodityList5'>￥51996.00<br/>22.88kg</li><li class='CommodityList6'><a class='#'>删除商品</a><a class='#'>加入我的关注</a></li></ul></div>";
-      document.querySelector("#body-cart").innerHTML += commidityInfoObj;
+
+
+
+
+
+
+
+
+
+
+/*数量加减按钮功能*/
+let buttonMinusGroup = document.getElementsByClassName("fa-minus");
+let buttonPlusGroup = document.getElementsByClassName("fa-plus");
+for (var i = 0; i < buttonMinusGroup.length; i++) {
+  buttonPlusGroup[i].onclick = function (event) {
+    let object = event.target.parentNode.parentNode.childNodes[1];
+      object.value ++
+      console.log(object.value);
+    let commidityID =  event.target.parentNode.parentNode.parentNode.parentNode.id;
+    for (var m = 0; m < commidityGroup.length; m++) {
+      if (commidityGroup[m].id == commidityID) {
+        commidityGroup[m].ordercount = object.value;
+
+      }
     }
   }
-  document.querySelector("#body-cart").innerHTML += endSignal;
+
+  buttonMinusGroup[i].onclick = function (event) {
+    let object = event.target.parentNode.parentNode.childNodes[1];
+    if (object.value > 1) {
+      object.value --
+    }
+    let commidityID =  event.target.parentNode.parentNode.parentNode.parentNode.id;
+    for (var m = 0; m < commidityGroup.length; m++) {
+      if (commidityGroup[m].id == commidityID) {
+        commidityGroup[m].ordercount = object.value;
+        console.log(commidityGroup[m].ordercount);
+      }
+    }
+  }
 }
